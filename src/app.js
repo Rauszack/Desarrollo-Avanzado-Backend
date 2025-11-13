0bacbb48ddec263d9dca60a4fd351d776376ec47
Nicolas Avellaneda <ne.avellaneda22@gmail.com>
2025-11-13 10:21:03 -0300

    agrego session

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cookieRoutes from './routes/cookieRoutes.js';
 import handlebars from 'express-handlebars';
 import {Server} from 'socket.io';
 import mongoose from 'mongoose';
@@ -9,12 +11,12 @@ import productRouter from './routes/productRouter.js';
 import cartRouter from './routes/cartRouter.js';
 import viewsRouter from './routes/viewsRouter.js';
 import __dirname from './utils/constantsUtil.js';
-import websocket from './websocket.js';
+import websocket from './websocket.js'
+import session from 'express-session';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 const PORT = process.env.PORT; 
 
 connectMongoDB();
@@ -24,15 +26,27 @@ app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'handlebars');
 
+
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(cookieParser(process.env.COOKIE_SECRET || 'fallbackSecret'));
+app.use(session({
+    secret: "sessionSecret",
+    resave: false,
+    saveUninitialized: false,    
+    cookie: { 
+        maxAge: 60000, 
+        httpOnly: true
+    }
+}));
 
 //Routers
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use('/', viewsRouter);
+app.use('/api/cookies', cookieRoutes);
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Start server in PORT ${PORT}`);
---
+++ src/routes/cookieRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/set/cookie', (req, res) => {
+  res.cookie('Codercookie', 'Cookie poderosa', {
+    maxAge: 900000, // 15 minutos
+    httpOnly: true
+  }).send("Cookie seteada");
+});
+
+router.get('/get/cookie', (req, res) => {
+  const cookieValue = req.cookies.Codercookie;
+  res.send(`Valor de la cookie: ${cookieValue}`);
+});
+
+router.get('/delete/cookie', (req, res) => {
+  res.clearCookie('Codercookie').send("Cookie eliminada");
+});
+
+export default router;
